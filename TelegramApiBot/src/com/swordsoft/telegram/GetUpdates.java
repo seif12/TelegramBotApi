f918d667bf9b8114106e2b2e6a2009e711bfaaec
seif12 <root.nucloide12@gmail.com>
2017-02-02 00:08:09 +0100

    21

---
+++ TelegramApiBot/src/com/swordsoft/telegram/GetUpdates.java
@@ -0,0 +1,62 @@
+package com.swordsoft.telegram;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TimerTask;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import com.swordsoft.utils.CnxUtils;
+
+public class GetUpdates extends TimerTask {
+
+	private static DataSource ds;
+
+	@Override
+	public void run() {
+		Context ctx;
+		try {
+			  ctx = new InitialContext();
+		
+		     ds = (DataSource)ctx.lookup("java:comp/env/jdbc/MySQLDS");
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	
+	Map<String,String> parameters = new HashMap<String,String>(); 
+	
+	parameters.put("url", "");
+	
+	System.out.println("Initiating Servlet ... ");
+	
+	Map<String,String> params = new HashMap<String,String>();
+	
+	params.put("limit","20");
+	
+	String resp =CnxUtils.initiatePost("https://api.telegram.org/bot320997687:AAEFMyZEhUso49cHojFndUnw3qtY3LnFwZA/getUpdates","" , params, false).toString();
+	
+	System.out.println(resp);
+	
+	try {
+		java.sql.Connection con = ds.getConnection();
+		PreparedStatement ps = con.prepareStatement("insert into log_table(log_text) values(?)" );
+		ps.setString(1, "update "+resp);
+		ps.executeUpdate();
+		ps.close();
+		con.close();
+		
+} catch (SQLException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
+		
+	}
+
+}
--- TelegramApiBot/src/com/swordsoft/telegram/WebHookTelegramApi.java
+++ TelegramApiBot/src/com/swordsoft/telegram/WebHookTelegramApi.java
@@ -1,22 +1,34 @@
 package com.swordsoft.telegram;
 
 import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Timer;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
+import com.mysql.jdbc.Connection;
 import com.swordsoft.utils.CnxUtils;
 
 
 public class WebHookTelegramApi extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-  
+	private DataSource ds;
+	
     public WebHookTelegramApi() {
         super();
         // TODO Auto-generated constructor stub
@@ -25,6 +37,28 @@ public class WebHookTelegramApi extends HttpServlet {
 	
 	public void init(ServletConfig config) throws ServletException {
 		
+	
+			Context ctx;
+			try {
+				  ctx = new InitialContext();
+			
+			     ds = (DataSource)ctx.lookup("java:comp/env/jdbc/MySQLDS");
+			} catch (NamingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		try {
+				java.sql.Connection con = ds.getConnection();
+				PreparedStatement ps = con.prepareStatement("insert into log_table(log_text) values(?)" );
+				ps.setString(1, "initialisation "+(new Date()));
+				ps.executeUpdate();
+				ps.close();
+				con.close();
+				
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		Map<String,String> parameters = new HashMap<String,String>(); 
 		
 		parameters.put("url", "");
@@ -33,11 +67,17 @@ public class WebHookTelegramApi extends HttpServlet {
 		
 		Map<String,String> params = new HashMap<String,String>();
 		
-		params.put("url","");
+		params.put("url","https://telegram-seif12.rhcloud.com/AAEFMyZEhUso49cHojFndUnw3qtY3LnFwZA");
 		
-		String resp =CnxUtils.initiatePost("https://api.telegram.org/bot320997687:AAEFMyZEhUso49cHojFndUnw3qtY3LnFwZA/setWebhook","" , null, true).toString();
+		String resp =CnxUtils.initiatePost("https://api.telegram.org/bot320997687:AAEFMyZEhUso49cHojFndUnw3qtY3LnFwZA/setWebhook","" , params, false).toString();
 		
 		System.out.println(resp);
+		
+		/*Timer t = new Timer();
+		GregorianCalendar gc = new GregorianCalendar();
+		gc.add(Calendar.SECOND, 15);
+		//Timer task t =;
+		t.scheduleAtFixedRate( new GetUpdates(), gc.getTime(), 15000);*/
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -45,7 +85,40 @@ public class WebHookTelegramApi extends HttpServlet {
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
+		try {
+				java.sql.Connection con = ds.getConnection();
+				String update="";
+				String line = "";
+				
+				PreparedStatement ps = con.prepareStatement("insert into log_table(log_text) values(?)" );
+				
+				do
+				{
+					 request.getReader().readLine();
+					update = update+(line!=null?line:"");
+				}
+				while(line != null);
+				ps.setString(1, "Post Request " +update );
+				ps.executeUpdate();
+				ps.close();
+				con.close();
+				
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		try{
+				java.sql.Connection con = ds.getConnection();
+				PreparedStatement ps = con.prepareStatement("insert into log_table(log_text) values(?)" );
+				ps.setString(1, "Post Request " + e.getMessage() );
+				ps.executeUpdate();
+				ps.close();
+				con.close();
+		
+			}
+		catch(Exception ex)
+		{
+			
+		}
+	
 
-}
+}}}
